deae10d12c8c92c6d442eec82eeea8578a91047a
root <root@DESKTOP-O5RKD4E.localdomain>
2021-10-19 17:23:11 -0300

    mudando docstring de ToStr

--- utils/toStr.py
+++ utils/toStr.py
@@ -6,9 +6,9 @@ class ToStr(beam.DoFn):
             Transforma uma tupla de listas em string formatada
     
             Args:
-                element (tupple): tupla (coduf, [[lista de regiao, governador, uf, estado, casosAcumulado, obitosAcumulado
+                element (tupple): tupla (coduf, [[regiao, uf, casosAcumulado, obitosAcumulado], [estado, governador]])
             Returns:
-                strings
+                string: regiao, estado, uf, governador, casosAcumulado, obitosAcumulado 
         '''
         if len(element[1][0]) > len(element[1][1]):
             lista = [element[1][0][0], element[1][0][1], element[1][1][1], element[1][1][0], element[1][0][2], element[1][0][3]]
